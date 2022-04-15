16d5a79b5179864e75810392d0f0b5be9339529b
fengxing103 <fengxkj103@gamil.com>
2022-04-15 12:20:29 +0800

    Initial commit

---
+++ app/src/main/java/com/bizzan/config/UrlConfig.java
@@ -0,0 +1,17 @@
+package com.bizzan.config;
+
+public class UrlConfig {
+
+    //    public static String BASE_URL="https://api.coingod.online";
+//    public static String BASE_URL="https://api.bizzan.pro";
+    public static String BASE_URL = "https://api.asdfl234.xyz";
+    //    public static String C2C_URL = "api.bizzan.pro";
+    public static String C2C_URL = "api.asdfl234.xyz";
+    //    public static String MARKET_URL = "api.bizzan.pro";
+    public static String MARKET_URL = "api.asdfl234.xyz";
+    //    public static String GROUP_URL = "api.bizzan.pro";
+    public static String GROUP_URL = "api.asdfl234.xyz";
+    //    public static String HEART_URL = "api.bizzan.pro";
+    public static String HEART_URL = "api.asdfl234.xyz";
+
+}
